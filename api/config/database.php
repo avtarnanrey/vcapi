5dd2c16b8e63ffb2be5158a720f2fd9aef9dbca7
Avtar Nanrey <avtar@avtarnanrey.com>
2018-01-23 05:54:05 -0500

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -1,7 +1,7 @@
 <?php
 class Database{
     private $host = 'localhost';
-    private $db_name = 'villec3ntral';
+    private $db_name = '*********';
     private $username = 'root';
     private $password = '';
     public $conn;
